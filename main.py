5cb3288ecb703bc7d62a33996168632d2e065957
Najeeriii <adrianomcle08@gmail.com>
2024-11-23 20:22:48 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Multiplication maker")
+number=int(input("Enter a number to see the table:"))
+for i in range(1,13,1):
+    print(number," x ",i," = ",number*i)
+name="Adriano"
+decimal=float(input("Enter to see table:"))
+for i in range(1,5,1):
+    print(decimal," * ",i," = ",decimal*i)
